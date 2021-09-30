d7f291ced2cde87733eddada510c18a4e205dc70
Mart Somermaa <mrts@users.noreply.github.com>
2021-09-30 17:25:21 +0300

    feat(OCSP): make collections immutable in AuthtokeValidationConfiguration.copy() and CertificateValidator.buildTrustAnchorsFromCertificates(), reimplement AIA OCSP access location retriving from certificate with higher level AuthorityInformationAccess API

--- src/main/java/org/webeid/security/validator/ocsp/OcspUrl.java
+++ src/main/java/org/webeid/security/validator/ocsp/OcspUrl.java
@@ -1,34 +1,36 @@
 /*
- * Copyright (c) 2017 The Netty Project
  * Copyright (c) 2020-2021 Estonian Information System Authority
  *
- * The Netty Project and The Web eID Project license this file to you under the
- * Apache License, version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package org.webeid.security.validator.ocsp;
 
-import org.bouncycastle.asn1.ASN1Encodable;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.ASN1Primitive;
-import org.bouncycastle.asn1.BERTags;
-import org.bouncycastle.asn1.DLSequence;
-import org.bouncycastle.asn1.DLTaggedObject;
-import org.bouncycastle.asn1.x509.Extension;
-import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
+import org.bouncycastle.asn1.ASN1String;
+import org.bouncycastle.asn1.x509.AccessDescription;
+import org.bouncycastle.asn1.x509.AuthorityInformationAccess;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.cert.X509CertificateHolder;
 
 import java.io.IOException;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.Objects;
 
@@ -37,73 +39,27 @@ public final class OcspUrl {
     public static final URI AIA_ESTEID_2015 = URI.create("http://aia.sk.ee/esteid2015");
 
     /**
-     * The OID for OCSP responder URLs.
-     * <p>
-     * https://oidref.com/1.3.6.1.5.5.7.48.1
-     */
-    private static final ASN1ObjectIdentifier OCSP_RESPONDER_OID
-        = new ASN1ObjectIdentifier("1.3.6.1.5.5.7.48.1").intern();
-
-    /**
      * Returns the OCSP responder {@link URI} or {@code null} if it doesn't have one.
      */
     public static URI getOcspUri(X509Certificate certificate) {
         Objects.requireNonNull(certificate, "certificate");
-        final byte[] value = certificate.getExtensionValue(Extension.authorityInfoAccess.getId());
-        if (value == null) {
-            return null;
-        }
-
-        final ASN1Primitive authorityInfoAccess;
+        final X509CertificateHolder certificateHolder;
         try {
-            authorityInfoAccess = JcaX509ExtensionUtils.parseExtensionValue(value);
-        } catch (IOException | IllegalArgumentException e) {
-            return null;
-        }
-        if (!(authorityInfoAccess instanceof DLSequence)) {
-            return null;
-        }
-
-        final DLSequence aiaSequence = (DLSequence) authorityInfoAccess;
-        final DLTaggedObject taggedObject = findObject(aiaSequence, OCSP_RESPONDER_OID, DLTaggedObject.class);
-        if (taggedObject == null) {
-            return null;
-        }
-
-        if (taggedObject.getTagNo() != BERTags.OBJECT_IDENTIFIER) {
-            return null;
-        }
-
-        final byte[] encoded;
-        try {
-            encoded = taggedObject.getEncoded();
-        } catch (IOException e) {
-            return null;
-        }
-        int length = encoded[1] & 0xFF;
-        final String uri = new String(encoded, 2, length, StandardCharsets.UTF_8);
-        return URI.create(uri);
-    }
-
-    private static <T> T findObject(DLSequence sequence, ASN1ObjectIdentifier oid, Class<T> type) {
-        for (final ASN1Encodable element : sequence) {
-            if (!(element instanceof DLSequence)) {
-                continue;
+            certificateHolder = new X509CertificateHolder(certificate.getEncoded());
+            final AuthorityInformationAccess authorityInformationAccess =
+                AuthorityInformationAccess.fromExtensions(certificateHolder.getExtensions());
+            for (AccessDescription accessDescription :
+                authorityInformationAccess.getAccessDescriptions()) {
+                if (accessDescription.getAccessMethod().equals(AccessDescription.id_ad_ocsp) &&
+                    accessDescription.getAccessLocation().getTagNo() == GeneralName.uniformResourceIdentifier) {
+                    final String accessLocationUrl = ((ASN1String) accessDescription.getAccessLocation().getName())
+                        .getString();
+                    return URI.create(accessLocationUrl);
+                }
             }
-
-            final DLSequence subSequence = (DLSequence) element;
-            if (subSequence.size() != 2) {
-                continue;
-            }
-
-            final ASN1Encodable key = subSequence.getObjectAt(0);
-            final ASN1Encodable value = subSequence.getObjectAt(1);
-
-            if (key.equals(oid) && type.isInstance(value)) {
-                return type.cast(value);
-            }
+        } catch (IOException | CertificateEncodingException | IllegalArgumentException | NullPointerException e) {
+            return null;
         }
-
         return null;
     }
 
