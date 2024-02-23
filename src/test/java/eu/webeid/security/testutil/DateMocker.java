2bc89ea784e4f07079d38b3bd37246f6e95142ef
Mart Somermaa <mrts@users.noreply.github.com>
2024-02-23 20:31:39 +0200

    fix: use system time in OcspResponseValidator.validateCertificateStatusUpdateTime()
    WE2-868
    
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- src/test/java/eu/webeid/security/testutil/DateMocker.java
+++ src/test/java/eu/webeid/security/testutil/DateMocker.java
@@ -23,19 +23,23 @@
 package eu.webeid.security.testutil;
 
 import com.fasterxml.jackson.databind.util.StdDateFormat;
+import eu.webeid.security.util.DateAndTime;
+import io.jsonwebtoken.Clock;
+import org.mockito.MockedStatic;
 
 import java.text.ParseException;
 import java.util.Date;
 
-public final class Dates {
+public class DateMocker {
+
     private static final StdDateFormat STD_DATE_FORMAT = new StdDateFormat();
 
-    public static Date create(String iso8601Date) {
+    public static void mockDate(String iso8601Date, MockedStatic<DateAndTime.DefaultClock> mockedClock) {
         try {
-            return STD_DATE_FORMAT.parse(iso8601Date);
+            final Date theDate = STD_DATE_FORMAT.parse(iso8601Date);
+            mockedClock.when(DateAndTime.DefaultClock::getInstance).thenReturn((Clock) () -> theDate);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
