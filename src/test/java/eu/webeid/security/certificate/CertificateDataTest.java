9c543d1a60e3f9541cae68895e7ce0b53d715493
Mart Somermaa <mrts@users.noreply.github.com>
2024-04-30 13:28:29 +0300

    Use Optional in CertificateData
    WE2-931
    
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- src/test/java/eu/webeid/security/certificate/CertificateDataTest.java
+++ src/test/java/eu/webeid/security/certificate/CertificateDataTest.java
@@ -2,12 +2,10 @@ package eu.webeid.security.certificate;
 
 import org.junit.jupiter.api.Test;
 
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
 import static eu.webeid.security.testutil.Certificates.getOrganizationCert;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CertificateDataTest {
 
@@ -15,24 +13,22 @@ class CertificateDataTest {
     void whenOrganizationCertificate_thenSubjectCNAndIdCodeAndCountryCodeExtractionSucceeds() throws Exception {
         final X509Certificate organizationCert = getOrganizationCert();
 
-        assertThat(CertificateData.getSubjectCN(organizationCert))
+        assertThat(CertificateData.getSubjectCN(organizationCert).orElseThrow())
             .isEqualTo("Testijad.ee isikutuvastus");
-        assertThat(CertificateData.getSubjectIdCode(organizationCert))
+        assertThat(CertificateData.getSubjectIdCode(organizationCert).orElseThrow())
             .isEqualTo("12276279");
-        assertThat(CertificateData.getSubjectCountryCode(organizationCert))
+        assertThat(CertificateData.getSubjectCountryCode(organizationCert).orElseThrow())
             .isEqualTo("EE");
     }
 
     @Test
-    void whenOrganizationCertificate_thenSubjectGivenNameAndSurnameExtractionFails() throws Exception {
+    void whenOrganizationCertificate_thenSubjectGivenNameAndSurnameAreEmpty() throws Exception {
         final X509Certificate organizationCert = getOrganizationCert();
 
-        assertThatThrownBy(() -> CertificateData.getSubjectGivenName(organizationCert))
-            .isInstanceOf(CertificateEncodingException.class)
-            .hasMessage("X500 name RDNs empty or first element is null");
-        assertThatThrownBy(() -> CertificateData.getSubjectSurname(organizationCert))
-            .isInstanceOf(CertificateEncodingException.class)
-            .hasMessage("X500 name RDNs empty or first element is null");
+        assertThat(CertificateData.getSubjectGivenName(organizationCert))
+            .isEmpty();
+        assertThat(CertificateData.getSubjectSurname(organizationCert))
+            .isEmpty();
     }
 
 }
