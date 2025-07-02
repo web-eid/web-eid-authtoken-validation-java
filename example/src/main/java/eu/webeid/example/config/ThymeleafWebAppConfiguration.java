4eb2ba0426f106783676987f7fb4264831fabf68
Sander.Kondratjev <sander.kondratjev@nortal.com>
2025-07-02 13:02:36 +0300

    NFC-47 Web eID for Mobile support for web-eid example
    Signed-off-by: Sander Kondratjev <sander.kondratjev@nortal.com>

--- example/src/main/java/eu/webeid/example/config/ThymeleafWebAppConfiguration.java
+++ example/src/main/java/eu/webeid/example/config/ThymeleafWebAppConfiguration.java
@@ -22,36 +22,16 @@
 
 package eu.webeid.example.config;
 
-import org.springframework.beans.factory.ObjectFactory;
-import eu.webeid.security.challenge.ChallengeNonce;
-import eu.webeid.security.challenge.ChallengeNonceStore;
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
-import jakarta.servlet.http.HttpSession;
+@Configuration
+public class ThymeleafWebAppConfiguration {
 
-public class SessionBackedChallengeNonceStore implements ChallengeNonceStore {
-
-    private static final String CHALLENGE_NONCE_KEY = "challenge-nonce";
-
-    final ObjectFactory<HttpSession> httpSessionFactory;
-
-    public SessionBackedChallengeNonceStore(ObjectFactory<HttpSession> httpSessionFactory) {
-        this.httpSessionFactory = httpSessionFactory;
+    @Bean
+    public JakartaServletWebApplication jakartaServletWebApplication(ServletContext servletContext) {
+        return JakartaServletWebApplication.buildApplication(servletContext);
     }
-
-    @Override
-    public void put(ChallengeNonce challengeNonce) {
-        currentSession().setAttribute(CHALLENGE_NONCE_KEY, challengeNonce);
-    }
-
-    @Override
-    public ChallengeNonce getAndRemoveImpl() {
-        final ChallengeNonce challengeNonce = (ChallengeNonce) currentSession().getAttribute(CHALLENGE_NONCE_KEY);
-        currentSession().removeAttribute(CHALLENGE_NONCE_KEY);
-        return challengeNonce;
-    }
-
-    private HttpSession currentSession() {
-        return httpSessionFactory.getObject();
-    }
-
 }
