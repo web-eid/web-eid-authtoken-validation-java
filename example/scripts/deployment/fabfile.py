2d8b39903c73ec162ce87d1b013a8b34733a5425
Mart Somermaa <mrts@users.noreply.github.com>
2025-07-11 14:06:32 +0300

    Add v2.7.0 release
    WE2-988
    
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- example/scripts/deployment/fabfile.py
+++ example/scripts/deployment/fabfile.py
@@ -13,6 +13,6 @@ def deploy(c):
     with c.cd(os.getenv('WEBEID_DIR')):
         c.run('git pull')
         c.run('mvn clean package com.google.cloud.tools:jib-maven-plugin:dockerBuild')
-        c.run('docker-compose down')
-        c.run('docker-compose up -d')
+        c.run('docker compose down')
+        c.run('docker compose up -d')
 
