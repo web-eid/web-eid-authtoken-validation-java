0f9f93f169be3ecbce9d51214335a6df20c34525
Mart Somermaa <mrts@users.noreply.github.com>
2025-07-11 14:06:32 +0300

    Add v2.7.0 release
    WE2-988
    
    Signed-off-by: Mart Somermaa <mrts@users.noreply.github.com>

--- example/scripts/deployment/fab.sh
+++ example/scripts/deployment/fab.sh
@@ -5,11 +5,10 @@ set -eu
 cd "$( dirname "$0" )"
 
 SERVER_ADDRESS=web-eid.eu
-SERVER_USER=baron
+SERVER_USER=username
 SERVER_PORT=22
 
-export WEBEID_DIR='web-eid/web-eid-springboot-example'
-export WEBEID_DIR='web-eid/test-web-eid-springboot-example'
+export WEBEID_DIR='web-eid/test-server-web-eid-authtoken-validation-java/example'
 
 . venv/bin/activate
-fab -e -H ${SERVER_USER}@${SERVER_ADDRESS}:${SERVER_PORT} "$@"
+~/.local/bin/fab -e -H ${SERVER_USER}@${SERVER_ADDRESS}:${SERVER_PORT} "$@"
