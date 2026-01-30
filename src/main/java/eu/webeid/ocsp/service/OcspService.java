4a1d7137e0589077703e574a33088b6d7d323fe8
Mart Aarma <mart.aarma@nortal.com>
2026-01-30 10:48:33 +0200

    AUT-2473 Add ResilientOcspCertificateRevocationChecker
    Co-authored-by: Madis Jaagup Laurson <madisjaagup.laurson@nortal.com>

--- src/main/java/eu/webeid/ocsp/service/OcspService.java
+++ src/main/java/eu/webeid/ocsp/service/OcspService.java
@@ -36,4 +36,8 @@ public interface OcspService {
 
     void validateResponderCertificate(X509CertificateHolder cert, Date now) throws AuthTokenException;
 
+    default OcspService getFallbackService() {
+        return null;
+    }
+
 }
