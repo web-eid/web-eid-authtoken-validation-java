c6f530fb39694a9c8283759f191f8113fd28f7ad
Sander Kondratjev <sander.kondratjev@nortal.com>
2026-03-11 10:27:39 +0200

    NFC-115 Implement minimal multiple signing certification solution
    Signed-off-by: Sander Kondratjev <sander.kondratjev@nortal.com>

--- src/main/java/eu/webeid/security/authtoken/UnverifiedSigningCertificate.java
+++ src/main/java/eu/webeid/security/authtoken/UnverifiedSigningCertificate.java
@@ -24,33 +24,27 @@ package eu.webeid.security.authtoken;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SupportedSignatureAlgorithm {
-    private String cryptoAlgorithm;
-    private String hashFunction;
-    private String paddingScheme;
+public class UnverifiedSigningCertificate {
 
-    public String getCryptoAlgorithm() {
-        return cryptoAlgorithm;
+    private String certificate;
+    private List<SupportedSignatureAlgorithm> supportedSignatureAlgorithms;
+
+    public String getCertificate() {
+        return certificate;
     }
 
-    public void setCryptoAlgorithm(String cryptoAlgorithm) {
-        this.cryptoAlgorithm = cryptoAlgorithm;
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
     }
 
-    public String getHashFunction() {
-        return hashFunction;
+    public List<SupportedSignatureAlgorithm> getSupportedSignatureAlgorithms() {
+        return supportedSignatureAlgorithms;
     }
 
-    public void setHashFunction(String hashFunction) {
-        this.hashFunction = hashFunction;
-    }
-
-    public String getPaddingScheme() {
-        return paddingScheme;
-    }
-
-    public void setPaddingScheme(String paddingScheme) {
-        this.paddingScheme = paddingScheme;
+    public void setSupportedSignatureAlgorithms(List<SupportedSignatureAlgorithm> supportedSignatureAlgorithms) {
+        this.supportedSignatureAlgorithms = supportedSignatureAlgorithms;
     }
 }
